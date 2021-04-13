3ea96a23348ca485911420a37104eeb81a0315e1
xm_mokangrun <mok123456>
2021-04-13 11:14:51 +0800

    修改布局,测试提交

--- app/src/main/java/huiluancard/com/mygitproject/MainActivity.java
+++ app/src/main/java/huiluancard/com/mygitproject/MainActivity.java
@@ -2,6 +2,7 @@ package huiluancard.com.mygitproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView viewById = findViewById(R.id.text2);
     }
 }
