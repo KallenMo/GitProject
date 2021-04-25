43850954592223ffc8ba0d8665736b97471b1e22
xm_mokangrun <mok123456>
2021-04-25 19:01:00 +0800

    更新MM2

---
+++ app/src/main/java/huiluancard/com/mygitproject/MM2.java
@@ -0,0 +1,17 @@
+package huiluancard.com.mygitproject;
+
+/**
+ * Description:
+ * Detail:
+ * Create Time: 2021/4/25
+ *
+ * @author kallen
+ * @version 1.0
+ * @see ...
+ * History:
+ * @since Since
+ */
+class MM2 {
+    int age;
+    String name;
+}
